f9ba2c7510b688575ba10f66ac8512b8134d3b9b
Marina Stratu <mstratu@student.42.us.org>
2020-06-08 14:57:04 -0700

    Update ImageList.js

--- web/React/pics/src/components/ImageList.js
+++ web/React/pics/src/components/ImageList.js
@@ -2,7 +2,7 @@ import React from 'react';
 
 const ImageList = (props) =>{
          const images = props.images.map((image) =>{
-            return <img key ={image}src = {image.urls.regular} alt="Image List"/> //fing the propertiesin network/all preview
+            return <img key ={image}src = {image.urls.regular} alt="Image List"/>
         });
     return<div><h3>IMAGE_LIST</h3>{images}</div>;
     
